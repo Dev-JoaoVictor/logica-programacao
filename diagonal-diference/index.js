7ff5e1665287ec30c1ba0ea5ad3ccf77f586029f
João <joaovictor.ramos34@gmail.com>
2024-02-26 10:21:54 -0300

    resolução do exercício

---
+++ diagonal-diference/index.js
@@ -0,0 +1,19 @@
+function diagonalDifference(arr) {
+  let leftDiagonal = 0;
+  let rightDiagonal = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    leftDiagonal += arr[i][i]
+    rightDiagonal += arr[i][arr.length - 1 - i]
+  }
+
+  return Math.abs(leftDiagonal - rightDiagonal)
+}
+
+let array = [
+  [11, 2, 4],
+  [4, 5, 6],
+  [10, 8, -12]
+]
+
+console.log(diagonalDifference(array))
